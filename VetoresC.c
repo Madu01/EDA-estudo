0be57909fd115cda7fa4069bd60d9b9aeb5b7630
Madu01 <64814266+Madu01@users.noreply.github.com>
2023-05-01 17:04:21 -0300

    add vetsg

--- VetoresC.c
+++ VetoresC.c
@@ -4,6 +4,7 @@
 
 int *pVetInt;
 float *pVetFloat;
+char *pVetChar;
 
 int* CriaVetInt(int qdtElemInt){
     pVetInt = (int*) calloc(qdtElemInt, sizeof(int));      
@@ -13,57 +14,61 @@ float* CriaVetFloat(int qdtElemFloat){
     pVetFloat = (float*) calloc(qdtElemFloat, sizeof(float));
 }
 
-
-
-
-
-char* vetInput(char *pvetcar, int ptammax, int *pqtde, char input){
-    if (pqtde != 0){
-        int indiceInput = pqtde + 1;
-        pvetcar[indiceInput] = input;
-    }
-    else{
-        pvetcar[pqtde] = input;
-    }
-    
+char* CriaVetChar(char *vet, int qdtElemInt){
+    vet = (char*) calloc((qdtElemInt), sizeof(char)); 
+    return vet;     
 }
 
-char* vetDeletElem(char *pvetcar, int ptammax, int pqtde, char caract1){
-    pvetcar = (char*) calloc(ptammax, sizeof(char));
-    for (int i = 0; i < pqtde; i++){
+
+void vetInput(char *pvetcar, int ptammax, int *pqtde, char input){
+    if ((*pqtde) == 0){
+        pvetcar[*pqtde] = input;
+        (*pqtde)++;
+    }
+    else{
+        pvetcar[*pqtde] = input;
+        (*pqtde)++;
+    }
+}
+
+void vetDeletElem(char *pvetcar, int ptammax, int *pqtde, char caract1){
+    for (int i = 0; i < (*pqtde); i++){
         char caract2 = pvetcar[i];
         if(caract1 == caract2){
-            pvetcar[i] = pvetcar[pqtde-1];
-            ptammax--;
+            pvetcar[i] = pvetcar[(*pqtde-1)];
+            (*pqtde)--;
         }
     }  
 }
 
 int main(int argc, char const *argv[])
 {
-    char *vet;
-    CriaVetInt(3);
-
-    vetInput(vet, 3, 0, 33);
-    vetInput(vet, 3, 1, 22);
-    vetInput(vet, 3, 2, 11);
-
-    printf("%s\n", vet);
-
-
-
     //a ideia é assim:
-    int *vet;
+    char *pVetChar;
+    char *pVetCharDel;
     int tamTotal = 3;
     int atual = 0;
-
-    CriaVetInt(tamTotal);
-    if (vetInput(vet, tamTotal, &atual, 33)){
-        
+    /* O que entra no parametro é uma cópia independente se a variavel é global, 
+    no caso se for usar um número que queira muda-lo dentro do método sempre usa o endereço
+    e no método um ponteiro, para que o valor real dele seja mudado de fato */
+    pVetChar = CriaVetChar(pVetChar, tamTotal);
+    vetInput(pVetChar, tamTotal, &atual, 'o'); //nesse caso a entrada que tem o caracter não precesa ser ponteiro pq não é string
+    vetInput(pVetChar, tamTotal, &atual, 'a');
+    vetInput(pVetChar, tamTotal, &atual, 'm');
+    vetDeletElem(pVetChar, tamTotal, &atual, 'a');
+    for (int i = 0; i < atual; i++){
+        printf("%c", pVetChar[i]);
     }
+    printf("\n");
+    vetInput(pVetChar, tamTotal, &atual, 'j');
+    vetInput(pVetChar, tamTotal, &atual, 'i');
+    for (int i = 0; i < atual; i++){
+        printf("%c", pVetChar[i]);
+    }
+    printf("\n");
     
-
-
+    
+    
 
 
     // CriaVetInt(4);
