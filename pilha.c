bb94e142e3f54bcfdc9cc28b4a40d3487a36fb74
Madu01 <64814266+Madu01@users.noreply.github.com>
2023-07-10 18:33:41 -0300

    Update pilha.c

--- pilha.c
+++ pilha.c
@@ -4,14 +4,24 @@
 
 // PILHA: LIFO (Last in, First out)
 // ============== PILHA SIMPLESMENTE ENCADEADA ===============
-typedef struct
+
+
+typedef struct TnoPS
 {
     int valor;
-    TnoPS *prox;
+    struct TnoPS *prox;
 } TnoPS;
 
-int vazia(TnoPS *pilha){
-    if (pilha == NULL)
+typedef struct 
+{
+    TnoPS *topo; // só consegue acessar o valor e prox da outra struct utilizando como tipo o TnoPS
+    unsigned int tamanho;
+} Pilha_enc;
+
+
+// Sem cabeça:
+int vaziaSemCab(TnoPS *topo){
+    if (topo->prox == NULL)
     {
         return 1; // true
     }
@@ -21,20 +31,51 @@ int vazia(TnoPS *pilha){
     
 }
 
-int empilha(TnoPS *topo, int num){
+int empilhaSemCab(TnoPS *topo, int num){
     TnoPS *novoNo;
-    if (vazia(topo) == 1)
+    if (vaziaSemCab(topo) == 1)
     {
-        topo = (TnoPS*) malloc(sizeof(TnoPS));
         topo->prox = NULL;
         topo->valor = num;
         return 1;
     }
-    else
-    {
+    if (vaziaSemCab(topo) == 0){
         novoNo = (TnoPS*) malloc(sizeof(TnoPS));
+        novoNo->valor = num;
         novoNo->prox = topo;
         topo = novoNo;
+        return 0;     
+    }
+    
+}
+// Com cabeça :
+int vaziaComCab(Pilha_enc *pilha){
+    if (pilha->tamanho == 0)
+    {
+        return 1; // true
+    }
+    else{
+        return 0; // false
+    }
+    
+}
+
+int empilhaComCab(Pilha_enc *pilha, int num){
+    TnoPS *novoNo;
+    if (vaziaComCab(pilha) == 1)
+    {
+        pilha->topo = (TnoPS*) malloc(sizeof(TnoPS));
+        pilha->topo->prox = NULL;
+        pilha->topo->valor = num;
+        pilha->tamanho++;
+        return 1;
+    }
+    if (vaziaComCab(pilha) == 0){
+        novoNo = (TnoPS*) malloc(sizeof(TnoPS));
+        novoNo->valor = num;
+        novoNo->prox = pilha->topo;
+        pilha->topo = novoNo;
+        pilha->tamanho++;
         return 0;     
     }
     
@@ -42,11 +83,24 @@ int empilha(TnoPS *topo, int num){
 
 int main(int argc, char const *argv[])
 {
+    /** sem cabeça:
     TnoPS *pilha;
+    pilha = (TnoPS*) malloc(sizeof(TnoPS)); 
     int returns;
-    returns = empilha(pilha, 2);
-    printf("%d", returns);
-    return 0;
+    returns = empilhaSemCab(pilha, 2);
+    printf("%d\n%d\n", returns, pilha->valor);
+    **/
+
+    // Com cabeça:
+    Pilha_enc *pilha;
+    pilha = (Pilha_enc*) malloc(sizeof(Pilha_enc));
+    pilha->tamanho = 0;
+    pilha->topo = NULL;
+    int returns;
+    returns = empilhaComCab(pilha, 3);
+    printf("%d\n%d\n", returns, pilha->topo->valor);
+
+    return 0;    
 }
 
 
